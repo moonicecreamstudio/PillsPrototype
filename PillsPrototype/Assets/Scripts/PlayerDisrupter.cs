2416dbc291ab84538bcf74d3aedb4b92dca0a235
moonicecreamstudio <moonicecreamstudio@gmail.com>
2026-01-20 21:07:04 -0500

    Shake to wake bar
    Changed how waking up works

--- PillsPrototype/Assets/Scripts/PlayerDisrupter.cs
+++ PillsPrototype/Assets/Scripts/PlayerDisrupter.cs
@@ -15,6 +15,8 @@ public class PlayerDisrupter : MonoBehaviour
     public RectTransform eyelid1;
     public RectTransform eyelid2;
     public PostProcessVolume volume;
+    public Slider wakeUpSlider;
+    public GameObject wakeUpSliderObject;
 
     [Header("Dozing Parameters")]
     public float _dozingSpeed;
@@ -26,6 +28,10 @@ public class PlayerDisrupter : MonoBehaviour
     public Vector2 eyelid1GoalPosition;
     public Vector2 eyelid2StartPosition;
     public Vector2 eyelid2GoalPosition;
+    public float currentWakeUpPoint;
+    public float requiredWakeUpPoints;
+    public float accelerationThreshold;
+    public Vector2 lastVelocity;
 
     [Header("Unfocused Parameters")]
     public float value;
@@ -39,30 +45,50 @@ public class PlayerDisrupter : MonoBehaviour
         {
             Debug.LogError("No volume profile");
         }
+        wakeUpSlider.maxValue = requiredWakeUpPoints;
+        wakeUpSliderObject.SetActive(false);
     }
     void Update()
     {
         // Hunger Bar Effect
 
-
         // Energy Bar Effects
         // When the player is tired, move the camera and shut eyelids
         if (playerStatusManager._isTired == true)
         {
-            Vector3 currentMousePosition = Input.mousePosition; // There's a bug that wakes the player up
+            wakeUpSliderObject.SetActive(true);
 
+            // Move the camera down
             current = Mathf.MoveTowards(current, 1, _dozingSpeed * Time.deltaTime);
             cameraController.xRotation = Mathf.Lerp(cameraController.xRotation, 90, curve.Evaluate(current));
 
+            // Close eyelids
             current2 = Mathf.MoveTowards(current2, 1, _eyelidSpeed * Time.deltaTime);
             Vector2 newPos1 = Vector2.Lerp(eyelid1StartPosition, eyelid1GoalPosition, curve.Evaluate(current2));
             eyelid1.anchoredPosition = newPos1;
-
             Vector2 newPos2 = Vector2.Lerp(eyelid2StartPosition, eyelid2GoalPosition, curve.Evaluate(current2));
             eyelid2.anchoredPosition = newPos2;
 
+            // Shake the mouse to wake up
+            Vector2 currentVelocity = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            Vector2 acceleration = currentVelocity - lastVelocity;
+            if (acceleration.magnitude > accelerationThreshold)
+            {
+                Debug.Log("Shaking Mouse");
+                currentWakeUpPoint += 1;
+            }
+            lastVelocity = currentVelocity;
+            wakeUpSlider.value = currentWakeUpPoint;
 
-            if (currentMousePosition != lastMousePosition)
+            // Old method
+            //Vector3 currentMousePosition = Input.mousePosition; // There's a bug that wakes the player up
+            //if (currentMousePosition != lastMousePosition)
+            //{
+            //    currentWakeUpPoint += 1;
+            //}
+            //lastMousePosition = currentMousePosition;
+
+            if (currentWakeUpPoint >= requiredWakeUpPoints)
             {
                 eyelid1.anchoredPosition = eyelid1StartPosition;
                 eyelid2.anchoredPosition = eyelid2StartPosition;
@@ -70,9 +96,13 @@ public class PlayerDisrupter : MonoBehaviour
                 current = 0f;
                 current2 = 0f;
                 Debug.Log("Wake up");
+                currentWakeUpPoint = 0;
+                wakeUpSlider.value = 0;
+                wakeUpSliderObject.SetActive(false);
             }
 
-            lastMousePosition = currentMousePosition;
+
+
         }
 
         // Calmness Bar Effect
