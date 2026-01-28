da415f2d76fead9ba735980defbde077906fe51e
TheSuperfon <astefanadon@gmail.com>
2026-01-28 01:36:17 -0500

    resumegame V1 implemented
    could still go with more resumes switching once button pressed (or at least more names)

--- PillsPrototype/Assets/Scripts/ResumeMinigame/ResumeCycle.cs
+++ PillsPrototype/Assets/Scripts/ResumeMinigame/ResumeCycle.cs
@@ -8,9 +8,10 @@ public class ResumeCycle : MonoBehaviour
     public GameObject requirments;
     public GameObject[] resumeList;
     public GameObject[] requirmentList;
-    int currentResume;
-    int prevResume;
-    int nextResume;
+    public int currentResume;
+    string firstquality;
+    string secondquality;
+    string thirdquality;
 
     // Start is called before the first frame update
     void Start()
@@ -47,7 +48,9 @@ public class ResumeCycle : MonoBehaviour
 
         EasyResumeRequirements();
 
+        //Debug.Log(resumeList.Length);
 
+        
     }
 
     void EasyResumeRequirements()
@@ -70,21 +73,26 @@ public class ResumeCycle : MonoBehaviour
             option2 = Random.Range(0, requirmentList.Length);
             option3 = Random.Range(0, requirmentList.Length);
             //chooseran = true;
-            Debug.Log(option3);
+            /*Debug.Log(option3);
             Debug.Log(option1);
-            Debug.Log(option2);
+            Debug.Log(option2);*/
         }
 
 
         
 
         requirmentList[option1].SetActive(true);
+        firstquality = requirmentList[option1].name;
         requirmentList[option1].GetComponent<RectTransform>().localPosition = new Vector3(0.07899928f, 0.282f, -0.5550001f);
 
         requirmentList[option2].SetActive(true);
         requirmentList[option2].GetComponent<RectTransform>().localPosition = new Vector3(0.07899928f, -0.022f, -0.5550001f);
+        secondquality = requirmentList[option2].name;
+
+
         requirmentList[option3].SetActive(true);
         requirmentList[option3].GetComponent<RectTransform>().localPosition = new Vector3(0.07899928f, -0.347f, -0.5550001f);
+        thirdquality = requirmentList[option3].name;
 
     }
 
@@ -95,9 +103,19 @@ public class ResumeCycle : MonoBehaviour
     void Update()
     {
         
-
-
-
+        //temp controls for buttons 
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            nextResumeVoid();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            PrevResumeVoid();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResumeAccept();
+        }
 
 
 
@@ -118,11 +136,11 @@ public class ResumeCycle : MonoBehaviour
     public void nextResumeVoid()
     {
 
-        Debug.Log("why");
+        //Debug.Log("why");
 
         resumeList[(currentResume)].gameObject.SetActive(false);
         currentResume += 1;
-        if (currentResume > resumeList.Length)
+        if (currentResume > resumeList.Length - 1)
         {
             currentResume = 0;
         }
@@ -134,7 +152,7 @@ public class ResumeCycle : MonoBehaviour
         currentResume -= 1;
         if (currentResume < 0)
         {
-            currentResume = resumeList.Length;
+            currentResume = resumeList.Length - 1;
         }
         resumeList[(currentResume)].gameObject.SetActive(true);
     }
@@ -142,14 +160,50 @@ public class ResumeCycle : MonoBehaviour
 
     public void ResumeAccept()
     {
+        bool ChoseCorrect = false;
+        GameObject chosenResume = null;
+        
+
+        for (int i = 0; i < resumeList.Length; i++)
+        {
+            if (resumeList[i].activeInHierarchy)
+            {
+                chosenResume = resumeList[i];
+            }
+            
+        }
 
 
+        GameObject[] resumeQualities = new GameObject[chosenResume.transform.childCount];
+        for (int i = 0; i < resumeQualities.Length; i++)
+        {
+            resumeQualities[i] = chosenResume.transform.GetChild(i).gameObject;
+            Debug.Log(resumeQualities[i]);
+            if ((resumeQualities[i].name == firstquality) || (resumeQualities[i].name == secondquality) || resumeQualities[i].name == thirdquality)
+            {
+                PlusPoint();
+                ChoseCorrect = true;
+                break;
+            }
+        }
 
+        if (ChoseCorrect == false)
+        {
+            Debug.Log("you lose something or other");
+        }
+        
+
+        //EasyResumeRequirements();
+
+    }
+
+    public void PlusPoint()
+    {
+        Debug.Log("you did it");
 
         EasyResumeRequirements();
 
     }
-
 
 
 }
---
+++ PillsPrototype/Assets/Scripts/SortingMinigame/PaperSortingScript1.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperSortingScript1 : MonoBehaviour
+{
+
+
+    public CameraController controller;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void spawnNewPaper()
+    {
+
+
+
+
+
+    }
+
+    public void redButton()
+    {
+
+
+    }
+
+    public void greenButton()
+    {
+
+
+    }
+
+    public void blueButton()
+    {
+
+
+
+    }
+
+}
