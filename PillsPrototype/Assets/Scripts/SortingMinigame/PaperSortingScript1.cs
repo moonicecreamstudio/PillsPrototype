ed82128df2e1e5de0f0fba95e1dcf3e09164b739
TheSuperfon <astefanadon@gmail.com>
2026-01-28 14:16:21 -0500

    papersorting in list and buttons setactive

--- PillsPrototype/Assets/Scripts/SortingMinigame/PaperSortingScript1.cs
+++ PillsPrototype/Assets/Scripts/SortingMinigame/PaperSortingScript1.cs
@@ -7,25 +7,48 @@ public class PaperSortingScript1 : MonoBehaviour
 
 
     public CameraController controller;
-
-
+    public GameObject ButtonCanvas;
+    public GameObject[] paperList;
+    public GameObject paperObjects;
+    int currentpaper;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ButtonCanvas.SetActive(false);
+        paperList = new GameObject[paperObjects.transform.childCount];
+
+        for (int i = 0; i < paperList.Length; i++)
+        {
+            paperList[i] = paperObjects.transform.GetChild(i).gameObject;
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            ButtonCanvas.SetActive(true);
+            spawnNewPaper();
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ButtonCanvas.SetActive(false);
+            paperList[currentpaper].SetActive(false);
+        }
+
+
+
     }
 
 
     public void spawnNewPaper()
     {
-
+        currentpaper = Random.Range(0, paperList.Length);
+        paperList[currentpaper].SetActive(true);
 
 
 
@@ -34,19 +57,48 @@ public class PaperSortingScript1 : MonoBehaviour
 
     public void redButton()
     {
+        if (paperList[currentpaper].name == "Red")
+        {
+            Debug.Log("you did it");
 
+        }
+        else
+        {
+            Debug.Log("try again");
+        }
 
+        spawnNewPaper();
     }
 
     public void greenButton()
     {
+        if (paperList[currentpaper].name == "Green")
+        {
+            Debug.Log("you did it");
 
+        }
+        else
+        {
+            Debug.Log("try again");
+        }
+
+        spawnNewPaper();
 
     }
 
     public void blueButton()
     {
+        if (paperList[currentpaper].name == "Blue")
+        {
+            Debug.Log("you did it");
 
+        }
+        else
+        {
+            Debug.Log("try again");
+        }
+
+        spawnNewPaper();
 
 
     }
