f63e1c726e9c86ee5aa98db705f8635c9e3b1b99
moonicecreamstudio <moonicecreamstudio@gmail.com>
2026-02-11 19:07:53 -0500

    Added level designs

--- PillsPrototype/Assets/Scripts/GameSettingsManager.cs
+++ PillsPrototype/Assets/Scripts/GameSettingsManager.cs
@@ -7,11 +7,23 @@ public class GameSettingsManager : MonoBehaviour
 {
     [Header("Reference")]
     public TextMeshProUGUI dayDisplayer;
+    public ClipBoardScript clipBoardScript;
+    public SliderManager dayTimer;
 
     [Header("Parameters")]
     public int maxDay;
     public static int currentDay { get; set; }
     public SceneTransitioner sceneTransitioner;
+
+    [System.Serializable]
+    public class LevelMetrics
+    {
+        public float secondsInLevel;
+        public float emailReq;
+        public float resumeReq;
+        public float sortReq;
+    }
+    public LevelMetrics[] levelMetrics;
 
     public void Awake()
     {
@@ -21,5 +33,17 @@ public class GameSettingsManager : MonoBehaviour
         {
             sceneTransitioner.SwitchScenes("GameOverScene");
         }
+
+        for (int i = 0; i < maxDay; i++)
+        {
+            if (currentDay == i)
+            {
+                dayTimer.secondsInLevel = levelMetrics[i].secondsInLevel;
+                clipBoardScript.EmailReq = levelMetrics[i].emailReq;
+                clipBoardScript.ResumeReq = levelMetrics[i].resumeReq;
+                clipBoardScript.PaperReq = levelMetrics[i].sortReq;
+            }
+        }
+
     }
 }
