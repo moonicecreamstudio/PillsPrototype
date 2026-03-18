8f8009eba97f731cb9b5717d2c57a8bdeca8cfdd
TheSuperfon <148352177+TheSuperfon@users.noreply.github.com>
2026-03-18 14:30:53 -0400

    UpdateOnschoolcomputer

--- PillsPrototype/Assets/Scripts/FocusTaskRelation.cs
+++ PillsPrototype/Assets/Scripts/FocusTaskRelation.cs
@@ -6,6 +6,8 @@ public class FocusTaskRelation : MonoBehaviour
 {
     PlayerStatusManager statusManager;
     public float difficultyLevel;
+    public PaperSortingScript1 sortingScript1;
+    public ResumeCycle ResumeCycle;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,20 +21,32 @@ public class FocusTaskRelation : MonoBehaviour
         if (statusManager.previousFocusValue >= (2 * statusManager.focusSlider.maxValue) / 3 && statusManager.focusSlider.value <= (2 * statusManager.focusSlider.maxValue) / 3) //thank you calvin. this is when slider is below 2/3 of the focuslider max value (aka stage 1)
         {
             Debug.Log("stage 1");
-            difficultyLevel = 1;
+            if (difficultyLevel != 1)
+            {
+                difficultyLevel = 1;
+            }
 
         }
-        else if (statusManager.previousFocusValue >= (statusManager.focusSlider.maxValue) / 3 && statusManager.focusSlider.value <= (statusManager.focusSlider.maxValue) / 3)
+        if (statusManager.previousFocusValue >= (statusManager.focusSlider.maxValue) / 3 && statusManager.focusSlider.value <= (statusManager.focusSlider.maxValue) / 3)
         {
+            if (difficultyLevel != 2)
+            {
+                difficultyLevel = 2; 
+            }
+
             Debug.Log("stage 2");
-            difficultyLevel = 2;
+            
         }
-        else if (statusManager.focusSlider.value <= statusManager.unfocusThreshold)
+        if (statusManager.focusSlider.value <= statusManager.unfocusThreshold)
         {
+            if (difficultyLevel != 3)
+            {
+                difficultyLevel = 3;
+            } 
             Debug.Log("stage 3");
-            difficultyLevel = 3;
+            
         }
-        else if (statusManager.focusSlider.value > (2 * statusManager.focusSlider.maxValue)/3)
+        if (statusManager.focusSlider.value > (2 * statusManager.focusSlider.maxValue)/3)
         {
             if (difficultyLevel != 0)
             {
